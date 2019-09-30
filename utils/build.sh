f4c9bbb19d74bb27446aa93e8029a67be407a389
Fredrik Hindersson <fredrik.hindersson@gmail.com>
2019-09-30 15:00:56 +0200

    Build only to dockerhub

--- utils/build.sh
+++ utils/build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-docker build -t brittlejf/docker/utils:latest .
-docker tag brittlejf/docker/utils registry.gitlab.com/brittlejf/docker/utils
+docker build -t brittlejf/utils:latest .
 
 read -p "Push image? (y/n): " -r
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-  docker push registry.gitlab.com/brittlejf/docker/utils:latest
+  docker push brittlejf/utils:latest
 fi
